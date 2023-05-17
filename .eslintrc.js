cf63e0e1c4404244824d482eda6f807f1a4336fc
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-17 15:08:58 +0900

    👔 add: eslint rule prettier tailwind-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,68 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
-};
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json', tsconfigRootDir: __dirname, sourceType: 'module' },
+  plugins: ['import-access', 'simple-import-sort', 'react-hooks'],
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
+    'no-restricted-syntax': ['error', { selector: 'TSEnumDeclaration', message: "Don't declare enums" }],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    // 関数の定義は関数式で書く↓
+    'func-style': ['error', 'expression'],
+    'arrow-body-style': ['error', 'always'],
+    'no-restricted-imports': ['error', { paths: [{ name: 'react', importNames: ['default'] }] }],
+    // typescriptで型チェックしてれば不要なのでoff
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'error',
+    'react/jsx-handler-names': [
+      'error',
+      {
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
+        checkLocalVariables: true,
+        checkInlineFunction: true,
+      },
+    ],
+    'react/destructuring-assignment': ['error', 'always'],
+
+    // import
+    'import/no-default-export': 'error',
+    'import-access/jsdoc': ['error'],
+    'import/newline-after-import': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+
+    // react-hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // typescript
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: ['typeAlias', 'typeParameter'], format: ['PascalCase'] },
+      { selector: ['classProperty', 'method'], format: ['camelCase'] },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['no', 'is', 'has', 'should'],
+        filter: { regex: '^_', match: false },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['src/app/**/*.{ts,tsx}'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  htmlWhitespaceSensitivity: 'strict',
+  trailingComma: 'es5',
+}
